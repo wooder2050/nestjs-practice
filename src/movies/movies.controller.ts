61e9faee16a730562e12bdac8d8fa385daf3a3d5
wooder2050 <wooder2050@gmail.com>
2021-04-01 19:18:14 +0900

    Update: 강의 완료

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,51 @@
+import { MoviesService } from './movies.service';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Patch,
+  Body,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYaer) {
+    return `We are searching for a moive made after : ${searchingYaer}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: number): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateData);
+  }
+}
